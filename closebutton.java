4154fbc3315beb8ef547c412b79bac28d4453929
Rahul Kumar S <91374700+RasCoder17@users.noreply.github.com>
2023-04-28 01:41:25 +0530

    Delete closebutton.java

--- closebutton.java
+++
@@ -1,8 +0,0 @@
-public class closebutton
-{
-int a = JOptionPane.showConfirmDialog(null,"Do you really want to exit Application ","select",JOptionPane.YES_NO_OPTION);
-        if (a == 0)
-        {
-            System.exit(0);
-        }
-    }        
