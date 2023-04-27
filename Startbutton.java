fbf34f918f9f7aea6c81722f765cdadd4179bccf
Rahul Kumar S <91374700+RasCoder17@users.noreply.github.com>
2023-04-28 01:45:15 +0530

    Delete Startbutton.java

--- Startbutton.java
+++
@@ -1,8 +0,0 @@
-public class startbutton
-{
-private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-    setVisible(false);
-    new quizExamStudent(rollNo).setVisible(true);
-   
-}
-}       
